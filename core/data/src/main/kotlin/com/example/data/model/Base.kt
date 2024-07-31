b05aec5c38ceacca56383ad5d4aa168c0d00b6d7
Sergei Karukes <ser1888kg@gmail.com>
2024-07-31 20:58:47 +0300

    фикс приведения типов

--- core/data/src/main/kotlin/com/example/data/model/Base.kt
+++ core/data/src/main/kotlin/com/example/data/model/Base.kt
@@ -3,43 +3,50 @@ package com.example.data.model
 import com.example.common.ui.UIElement
 
 public inline fun <reified T> createProfileElement(element: UIElement): ProfileElement {
-    val profileElement = when (T::class) {
-        Button::class -> Button(
-            Id = element.id,
-            Label = element.label ?: "",
-            Hide = !element.isVisible,
-            Disabled = !element.isEnabled,
-            Action = null
-        ) as ProfileElement
-        LastName::class -> LastName(
-            Id = element.id,
-            Label = element.hint ?: "",
-            Hide = !element.isVisible,
-            Disabled = !element.isEnabled,
-            IgnoreCustomerData = false,
-            Validators = emptyList(),
-            Field = element.field
-        ) as ProfileElement
-        Gender::class -> Gender(
-            Id = element.id,
-            Label = element.label ?: "",
-            Hide = !element.isVisible,
-            Disabled = !element.isEnabled,
-            IgnoreCustomerData = false,
-            SupportValues = element.options ?: emptyList(),
-            GenderValueMaps = emptyList(),
-            Field = element.field
-        ) as ProfileElement
-        Birthday::class -> Birthday(
-            Id = element.id,
-            Label = element.hint ?: "",
-            Hide = !element.isVisible,
-            Disabled = !element.isEnabled,
-            IgnoreCustomerData = false,
-            Validators = emptyList(),
-            Field = element.field
-        ) as ProfileElement
+    return when (T::class) {
+        Button::class -> ProfileElement(
+            Button = Button(
+                Id = element.id,
+                Label = element.label ?: "",
+                Hide = !element.isVisible,
+                Disabled = !element.isEnabled,
+                Action = null
+            )
+        )
+        LastName::class -> ProfileElement(
+            LastName = LastName(
+                Id = element.id,
+                Label = element.hint ?: "",
+                Hide = !element.isVisible,
+                Disabled = !element.isEnabled,
+                IgnoreCustomerData = false,
+                Validators = emptyList(),
+                Field = element.field
+            )
+        )
+        Gender::class -> ProfileElement(
+            Gender = Gender(
+                Id = element.id,
+                Label = element.label ?: "",
+                Hide = !element.isVisible,
+                Disabled = !element.isEnabled,
+                IgnoreCustomerData = false,
+                SupportValues = element.options ?: emptyList(),
+                GenderValueMaps = emptyList(),
+                Field = element.field
+            )
+        )
+        Birthday::class -> ProfileElement(
+            Birthday = Birthday(
+                Id = element.id,
+                Label = element.hint ?: "",
+                Hide = !element.isVisible,
+                Disabled = !element.isEnabled,
+                IgnoreCustomerData = false,
+                Validators = emptyList(),
+                Field = element.field
+            )
+        )
         else -> throw IllegalArgumentException("Unsupported type")
     }
-    return ProfileElement::class.constructors.first().call(profileElement)
 }
