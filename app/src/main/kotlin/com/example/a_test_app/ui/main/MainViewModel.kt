a8b7e24281a4dc19597d02d2d54a7b66c8aa5e0f
Sergei Karukes <ser1888kg@gmail.com>
2024-07-31 03:22:35 +0300

    Переписал viewModel на State

--- app/src/main/kotlin/com/example/a_test_app/ui/main/MainViewModel.kt
+++ app/src/main/kotlin/com/example/a_test_app/ui/main/MainViewModel.kt
@@ -1,12 +1,11 @@
 package com.example.a_test_app.ui.main
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.a_test_app.ui_logic.main.GetProfileUseCase
+import com.example.a_test_app.ui_logic.main.MainUIState
 import com.example.a_test_app.ui_logic.main.UpdateProfileUseCase
 import com.example.common.ui.ElementType
-import com.example.common.ui.UIElement
 import com.example.data.model.RootJSON
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -15,19 +14,13 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class ProfileViewModel @Inject constructor(
+class MainViewModel @Inject constructor(
     private val getProfile: GetProfileUseCase,
     private val updateProfile: UpdateProfileUseCase
 ) : ViewModel() {
 
-    private val _profile = MutableStateFlow<RootJSON?>(null)
-    val profile: StateFlow<RootJSON?> get() = _profile
-
-    private val _uiElements = MutableStateFlow<List<UIElement>>(emptyList())
-    val uiElements: StateFlow<List<UIElement>> get() = _uiElements
-
-    private val _isEditing = MutableStateFlow(false)
-    val isEditing: StateFlow<Boolean> get() = _isEditing
+    private val _uiState = MutableStateFlow(MainUIState())
+    val uiState: StateFlow<MainUIState> get() = _uiState
 
     init {
         loadProfileData()
@@ -40,45 +33,48 @@ class ProfileViewModel @Inject constructor(
         }
     }
 
-    fun updateProfileElements(profile: RootJSON?) {
+    private fun updateProfileElements(profile: RootJSON?) {
         profile?.let {
-            _profile.value = profile
-            _uiElements.value = convertProfileToUIElements(profile.ProfileElements)
+            _uiState.value = _uiState.value.copy(
+                profile = profile,
+                uiElements = convertProfileToUIElements(profile.ProfileElements)
+            )
         }
     }
 
-    fun isEditing(state: Boolean) {
-        _isEditing.value = state
-        _uiElements.value = _uiElements.value.map { element ->
-            if (
-                element.type == ElementType.EDIT_TEXT ||
-                element.type == ElementType.EDIT_TEXT_CALENDAR ||
-                element.type == ElementType.SPINNER
-            ) {
-                element.copy(isEnabled = state)
-            } else {
-                element
+    fun setEditing(state: Boolean) {
+        _uiState.value = _uiState.value.copy(
+            isEditing = state,
+            uiElements = _uiState.value.uiElements.map { element ->
+                if (
+                    element.type == ElementType.EDIT_TEXT ||
+                    element.type == ElementType.EDIT_TEXT_CALENDAR ||
+                    element.type == ElementType.SPINNER
+                ) {
+                    element.copy(isEnabled = state)
+                } else {
+                    element
+                }
+            }.map {
+                when (it.id) {
+                    "button_edit_id" -> it.copy(isVisible = !state)
+                    "button_save_id" -> it.copy(isVisible = state)
+                    "button_cancel_id" -> it.copy(isVisible = state)
+                    else -> it
+                }
             }
-        }
-        _uiElements.value = _uiElements.value.map {
-            when (it.id) {
-                "button_edit_id" -> it.copy(isVisible = !state)
-                "button_save_id" -> it.copy(isVisible = state)
-                "button_cancel_id" -> it.copy(isVisible = state)
-                else -> it
-            }
-        }
+        )
     }
 
     fun saveProfile() {
         viewModelScope.launch {
             val updatedProfile = RootJSON(
-                TestMode = _profile.value?.TestMode ?: false,
-                EmulationMode = _profile.value?.EmulationMode ?: false,
-                ProfileElements = convertUIElementsToProfile(_uiElements.value)
+                TestMode = _uiState.value.profile?.TestMode ?: false,
+                EmulationMode = _uiState.value.profile?.EmulationMode ?: false,
+                ProfileElements = convertUIElementsToProfile(_uiState.value.uiElements)
             )
             updateProfile.update(updatedProfile)
-            isEditing(false)
+            setEditing(false)
         }
     }
 }
