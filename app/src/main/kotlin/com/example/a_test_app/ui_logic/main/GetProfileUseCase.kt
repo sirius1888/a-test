239713544e7d2ede17ff7c8696fed2426495762d
Sergei Karukes <ser1888kg@gmail.com>
2024-07-31 17:57:08 +0300

    Убрал лишние импорты

--- app/src/main/kotlin/com/example/a_test_app/ui_logic/main/GetProfileUseCase.kt
+++ app/src/main/kotlin/com/example/a_test_app/ui_logic/main/GetProfileUseCase.kt
@@ -3,7 +3,6 @@ package com.example.a_test_app.ui_logic.main
 import com.example.a_test_app.ui.main.ProfileMapper
 import com.example.common.ui.UIElement
 import com.example.data.ProfileRepository
-import com.example.data.model.RootJSON
 import javax.inject.Inject
 
 class GetProfileUseCase @Inject constructor(
